6924de86ad2dc53a2f289b7d36a3e5afe79b9c36
Administrator <Administrator@ADMIN>
2019-04-09 11:40:32 +0700

    xong trang 59

--- BigSchool/ViewModels/CourseViewModel.cs
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -9,16 +9,38 @@ namespace BigSchool.ViewModels
 {
     public class CourseViewModel
     {
-        [Required]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Địa điểm không được để trống")]
         public string Place { get; set; }
-        [Required]
-        [FutureDate]
+
+
+
+        [Required(ErrorMessage = "Ngày không được để trống")]
+        [FutureDate(ErrorMessage = "Ngày tháng năm phải lớn hơn ngày tháng năm ở hiện tại")]
         public string Date { get; set; }
-        [Required]
-        [ValidTime]
+   
+
+
+        [Required(ErrorMessage = "Thời gian không được để trống")]
+        [ValidTime(ErrorMessage = "Định dạng sai (hh:mm)")]
         public string Time { get; set; }
+
+
+
+        [Required(ErrorMessage = "Vui lòng chọn Thể loại")]
         public byte Category { get; set; }
+
+
         public IEnumerable<Category> Categories { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Action
+        {
+            get { return (Id != 0) ? "Update" : "Create"; }
+        }
         public DateTime GetDateTime()
         {
             return DateTime.Parse(string.Format("{0} {1}", Date, Time));
